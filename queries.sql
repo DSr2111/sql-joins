407056367e3ae41499b548e45d1c50f900077dab
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-25 01:13:14 -0500

    q2

--- queries.sql
+++ queries.sql
@@ -2,3 +2,12 @@
 SELECT * owners AS o
 FULL OUTER JOIN vehicles AS v
 ON o.id = v.owner_id;
+
+--q2--
+SELECT first_name, last_name, 
+COUNT(owner_id) from owners AS o 
+JOIN vehicles AS v ON o.id=v.owner_id 
+GROUP BY (first_name, last_name)
+ORDER BY first_name;
+
+
