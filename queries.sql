40e1ea58aa96b5590d2031a1492812680aa8c2a9
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-25 01:22:11 -0500

    attempt from information given

--- queries.sql
+++ queries.sql
@@ -10,4 +10,9 @@ JOIN vehicles AS v ON o.id=v.owner_id
 GROUP BY (first_name, last_name)
 ORDER BY first_name;
 
+--q3--
+SELECT first_name, last_name,
+WHERE COUNT(vehicles) > 1
+AVG(price) > 10000
+ORDER BY first_name DESC
 
